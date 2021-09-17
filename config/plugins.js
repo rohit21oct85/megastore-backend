f7be7098d3a53724e26859c521076041899cf0a8
Rohit Singh <rohit.singh@firstprinciples.io>
2021-09-17 11:36:51 +0530

    add cloudnary

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,18 @@
-module.exports = ({ env }) => ({
-      email: {
-            provider: 'sendgrid',
-            providerOptions: {
-                  apiKey: env('SENDGRID_API_KEY'),
-            },
-            settings: {
-            defaultFrom: 'juliasedefdjian@strapi.io',
-            defaultReplyTo: 'juliasedefdjian@strapi.io',
-            testAddress: 'juliasedefdjian@strapi.io',
-            },
-      },
+module.exports = ({ env }) => {
+      if(env('NODE_ENV') === 'production'){
+            return {
+                  upload: {
+                        provider: 'cloudinary',
+                        providerOptions: {
+                              cloud_name: env('CLOUDINARY_NAME'),
+                              api_key: env('CLOUDINARY_KEY'),
+                              api_secret: env('CLOUDINARY_SECRET'),
+                        },
+                  },
+            }
+      }
+      return {
+            
+      }
       
-});
+};
